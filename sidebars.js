2790ba66496e09c15d72d025a4f0e4efe6b65e69
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-25 02:30:59 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -5,13 +5,23 @@ module.exports = {
         type: "category",
         label: "Framework",
         items: [
+          {
+             type: "category",
+             label: "API",
+             items: [
+            "tfx_api/index",
+            "tfx_api/loading/index",
+            "tfx_api/loading/setup/index",
+            "tfx_api/loading/install/index",
+          ],
+         },
            {
              type: "category",
              label: "Commands",
              items: [
-            "commands/index",
-            "commands/economy/index",
-            "commands/jobs/index",
+            "tfx_framework/commands/index",
+            "tfx_framework/commands/economy/index",
+            "tfx_framework/commands/jobs/index",
           ],
          },
         ],
